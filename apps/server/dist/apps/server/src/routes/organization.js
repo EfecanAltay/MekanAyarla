ca4971a9228052365790b889bf1ec649ade79ec1
Efecan <efecan95.gs@gmail.com>
2026-04-12 15:21:00 +0300

    chore: apply gitignore and remove tracked generated files

--- apps/server/dist/apps/server/src/routes/organization.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const organization_1 = require("../controllers/organization");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.use(auth_1.authenticate);
-router.get('/metadata', organization_1.getOrganizationMetadata);
-exports.default = router;
